c80b9de685481be2d597865a43acf8b6b40cf3db
Edward Anderson <ejanderson4@gmail.com>
2013-05-16 17:27:49 -0500

    attempting to get facebook login working

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515202234) do
+ActiveRecord::Schema.define(:version => 20130516214554) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -184,6 +183,8 @@ ActiveRecord::Schema.define(:version => 20130515202234) do
     t.string   "city"
     t.string   "state"
     t.integer  "level",                  :default => 0
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
