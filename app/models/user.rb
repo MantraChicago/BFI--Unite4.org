d842d95b3c92a29d275244eae5efa95c1999aec9
Edward Anderson <ejanderson4@gmail.com>
2013-05-17 10:10:47 -0500

    facebook login debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    logger.debug auth.extra.raw_info.inspect
     unless user
       user = User.create(:name => auth.extra.raw_info.name,
                            :provider => auth.provider,
