1ace15ec709f5796935609cf4b04fe18f060d050
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-12 17:42:36 -0500

    Removing test directory

--- test/functional/causeneeds_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CauseneedsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/user_causes_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserCausesTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
