8a4c863f7d83b8b42867536f078f93b11f0829d1
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-13 16:35:42 -0500

    Adding queryable module documentation

--- lib/queryable.rb
+++ lib/queryable.rb
@@ -2,6 +2,9 @@ module Queryable
   extend ActiveSupport::Concern
 
   module ClassMethods
+    # It is expected that each model class will implement
+    # its own query method, which handles all of the various
+    # parameters that make sense for that resource
     def query(params={})
       scoped = {}
     end
