dc4fc5799e3540f9b5df78076d6d959e371fb4f4
Eddie Anderson <ejanderson4@gmail.com>
2013-06-21 17:54:45 -0500

    active admin needs can pick cause

--- app/admin/good_needs.rb
+++ app/admin/good_needs.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register GoodNeed do
 	form :html => { :enctype => "multipart/form-data" } do |f|
 	   f.inputs "Details" do
-	    
+	   	f.input :cause, :as => :select, :collection => Cause.all, :include_blank => false
 	    f.input :name
 	    f.input :description
-	    f.input :cause_id
+	    
 	    f.input :address
+
 	    
 	    f.input :picture, :as => :file, :hint => f.template.image_tag(f.object.picture.url(:medium))
 	    
--- app/models/needs/need.rb
+++ app/models/needs/need.rb
@@ -1,6 +1,5 @@
 class Need < ActiveRecord::Base
   attr_accessible :name,  :cause_id, :description, :picture, :address, :start_date, :end_date
-  #belongs_to :needable, :polymorphic => true
   belongs_to :cause
 
   
