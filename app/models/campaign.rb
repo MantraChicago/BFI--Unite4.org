0d90ed76f30aa896b5555f46c57749655285563e
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-24 12:59:21 -0500

    Adding active/inactive campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,7 @@
 class Campaign < ActiveRecord::Base
   belongs_to :cause
   belongs_to :need
+
+  scope :active, lambda { where(active:true) }
+  scope :inactive, lambda { where(active:false) }
 end
