efcdf79755101b9d6709b9163c55077f68b06366
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-24 10:07:26 -0500

    Revert "cleaning up needs models"
    This reverts commit 75fa4ef9a4787376d9d60d22300e483eb47098b7.

---
+++ app/models/needs/volunteer_need.rb
@@ -0,0 +1,25 @@
+class VolunteerNeed < Need
+  has_many :volunteers
+  #has_many :needs, :as => :needable
+end
+
+# == Schema Information
+#
+# Table name: needs
+#
+#  id                   :integer          not null, primary key
+#  name                 :string(255)
+#  type                 :string(255)
+#  cause_id             :integer
+#  description          :text
+#  start_date           :datetime
+#  end_date             :datetime
+#  address              :string(255)
+#  picture_file_name    :string(255)
+#  picture_content_type :string(255)
+#  picture_file_size    :integer
+#  picture_updated_at   :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
