f7720876d7d608be033691a61c8bf2a2a13e0167
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-24 11:16:21 -0500

    removing test directory, adding campaign relationship to cause

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -5,9 +5,11 @@ class Cause < ActiveRecord::Base
   attr_accessible :cause_type_ids, :city, :state, :picture, :is_featured, :description, :twitter_handle, :video_link, :name, :mission_statement, :how_hear, :phone_number, :email, :website, :facebook_url
   has_attached_file  :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/missing.jpeg"
 
-  has_many :needs
   has_and_belongs_to_many :cause_types, :join_table => 'causes_cause_types'
-  has_many :locations, :dependent => :delete_all
+
+  has_many :needs, :dependent => :destroy_all
+  has_one :campaign
+  has_many :locations, :dependent => :destroy_all
 
   validates :name, :uniqueness => true
 
