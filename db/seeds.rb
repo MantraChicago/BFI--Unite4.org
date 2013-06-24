5d115246d8f61711f520001f65153c0a985ecc44
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-06-24 18:33:14 -0500

    updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+[
+  Cause,
+  Campaign,
+  Need,
+  FeaturedCause
+].each &:delete_all
 
-causeTypes=[
-	"Arts", 
-	'Education' ,
-	'Environment' ,
-	'Health & Wellness' ,
-	'Youth' ,
-	'Religion' ,
-	'Global Outreach',
-	'Social Equality']
-causeTypes.each do |type|
-	CauseType.find_or_create_by_name(type)
-end
 
-gameBadges=[{name: 'Complete profile', file_name:'', accomplishment: "level_0"},
-			{name: 'Watch Video', file_name:'', accomplishment: "level_1"},
-			{name: 'Share with friends', file_name:'',accomplishment: "level_2"}
-			]
-gameBadges.each do |badgeObj|
-	badge=Badge.find_or_create_by_accomplishment badgeObj[:accomplishment]
-	badge.update_attributes badgeObj
-end
