0bc0d858344728608b474b914c2547119feed15e
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-05 22:55:29 -0500

    removing annotations from specs

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -20,23 +20,3 @@ describe Location do
     location.should be_should_geocode
   end
 end
-
-# == Schema Information
-#
-# Table name: locations
-#
-#  id               :integer          not null, primary key
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  cause_id         :integer
-#  latitude         :float
-#  longitude        :float
-#  name             :string(255)
-#  address_line_one :string(255)
-#  address_line_two :string(255)
-#  city             :string(255)
-#  region           :string(255)
-#  postal_code      :string(255)
-#  country          :string(255)
-#
-
