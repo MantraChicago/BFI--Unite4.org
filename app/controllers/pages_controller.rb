6623a3106c234fb3ee62c97df9629f2ba910ad37
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-05 22:55:57 -0500

    cleaning up controllers and helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,4 @@
 class PagesController < ApplicationController
 	def index
-		@subscribe = Subscribe.new
-		@new_causes = Cause.order('created_at asc').limit(4);
-		@featured_causes=Cause.find(:all, :order => 'RANDOM()', :limit=>3, :conditions=>{:is_featured=>true})
-		@recent_needs=Need.order('created_at asc').limit(4);
-
-		
-		@featured_users = User.random 2
-		@campaigns=Campaign.find(:all, :order => 'RANDOM()', :limit=>4)
 	end
-
-
 end
