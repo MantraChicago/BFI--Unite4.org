9223abd299fee8375ea2aed044b6f109b60ff710
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-05 22:55:14 -0500

    improvement around featured cause functionality

--- app/models/featured_cause.rb
+++ app/models/featured_cause.rb
@@ -6,19 +6,27 @@ class FeaturedCause < ActiveRecord::Base
   has_many :causes
 
   scope :top_three, lambda { limit(3).reorder("updated_at DESC") }
+  scope :top_four, lambda { limit(4).reorder("updated_at DESC") }
+
+  def self.featured count=4
+    top_four
+  end
+
+  def self.cause_ids
+    scoped.select("distinct cause_id").map(&:cause_id)
+  end
 
   # can't really query this model
   def self.query(params={})
-    top_three
+    top_four
+  end
+
+  def self.populate!
+    delete_all
+    causes = Cause.reorder("random()").limit(4)
+
+    causes.each do |cause|
+      create(cause_id: cause.id)
+    end
   end
 end
-
-# == Schema Information
-#
-# Table name: featured_causes
-#
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
