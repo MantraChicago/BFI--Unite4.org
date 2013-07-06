39e6a16a083e8150f479fc212d2fc8024588626a
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-06 04:02:15 -0500

    adding hub pages, adding images for hub pages through naming convention in app/assets/images

--- app/controllers/category_hubs_controller.rb
+++ app/controllers/category_hubs_controller.rb
@@ -1,5 +1,5 @@
 class CategoryHubsController < ApplicationController
   def show
-
+    @cause_type = CauseType.find_by_slug(params[:cause_type_slug])
   end
 end
--- app/helpers/cache_key_helper.rb
+++ app/helpers/cache_key_helper.rb
@@ -30,8 +30,14 @@ module CacheKeyHelper
     "causes/by_city_and_cause_type-#{ city_slug }-#{ cause_type_slug }-#{ count }-#{ maximum_updated_at }"
   end
 
-  def cache_key_for_cause_types_by_city city_slug
-    count = Cause.where(:city_slug => city_slug).count
+  def cache_key_for_cities_grouped_by_cause_type cause_type_slug
+    count = Cause.by_cause_type(cause_type_slug).count
+    maximum_updated_at = Cause.by_cause_type(cause_type_slug).maximum(:updated_at)
+    "cities/grouped-by-cause-type/#{ cause_type_slug }-#{ count }-#{ maximum_updated_at }"
+  end
+
+  def cache_key_for_cause_types_grouped_by_city city_slug
+    count = Cause.by_city_slug(city_slug).count
     maximum_updated_at = CauseType.joins(:causes).where("causes.city_slug = ?", city_slug).maximum(:updated_at).try(:utc).try(:to_s, :number)
     "cause_types/grouped-by-city/#{ city_slug }-#{ count }-#{ maximum_updated_at }"
   end
