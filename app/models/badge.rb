f2fac7ade4a51f17d05b918bc0311655dcc83f33
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-05 22:53:42 -0500

    removing annotations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -14,17 +14,3 @@ class Badge < ActiveRecord::Base
     results
   end
 end
-
-# == Schema Information
-#
-# Table name: badges
-#
-#  id             :integer          not null, primary key
-#  name           :string(255)
-#  file_name      :string(255)
-#  accomplishment :string(255)
-#  description    :text
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#
-
