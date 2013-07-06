9ae567d2c3098caa786b1103af41a9270c69088b
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-05 22:55:57 -0500

    cleaning up controllers and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def recordAction action, params={}
-    if current_user
-      $customerio.track(current_user.id, action, params) 
-    end   
-  end
+  helper :all
 
 end
