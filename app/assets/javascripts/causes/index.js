3344290c54485f19d3908fd52e3e9b07aee36eef
Eddie Anderson <ejanderson4@gmail.com>
2013-07-08 11:56:20 -0500

    adding version 1 map on causes/index. Styling still needed

---
+++ app/assets/javascripts/causes/index.js
@@ -0,0 +1,38 @@
+  
+  var needsCollection=parseObjIntoModels(needsObj, Backbone.Model.extend({
+    name:'needs'
+    }))
+  var causesCollection=parseObjIntoModels(causesObj, Cause)
+    var locationsCollection=parseObjIntoModels(locationsObj, Location)
+
+    var map =createGoogleMap($('#map-canvas')[0])
+
+    populateMapMarkers(map,locationsCollection.models)
+
+    var filterOptions={'Causes':{'models':causesCollection.models,
+      'filterFunction':function(model,id){
+        return model.get('cause_id')==id
+      }
+
+  }}
+
+  /*
+  var modelNames = ['DonationsNeed', 'GoodNeed', 'VolunteerNeed'] 
+  for(var i in modelNames ){
+    var modelName= modelNames[i]
+    filterOptions[modelName.replace('N',' N')]={
+      'models':needsCollection.filter(function(model){ return model.get('type')==modelName}),
+        'filterFunction':function(model,id){
+        var need=needsCollection.get(id)
+        return model.get('cause_id')==need.get('cause_id')
+        }
+
+
+    }
+  }
+  */
+
+    var filterView = new FilterView({
+      el: $('#accordian-holder'), 
+      locationsCollection:locationsCollection,
+      filterOptions:filterOptions })
