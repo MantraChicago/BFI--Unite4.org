dfa1d4aca03007df53044ea89889fdc168814250
Eddie Anderson <ejanderson4@gmail.com>
2013-07-09 21:14:55 -0500

    wizard jQuery plugin

--- app/assets/javascripts/components/wizard.js
+++ app/assets/javascripts/components/wizard.js
@@ -1,7 +1,32 @@
-var WizardView=Backbone.View.extend({
-  initialize:function(){
-    this.render()
-  },render: function(){
+!function ($) {
+    $.fn.MT_wizard = function(direction) {
+      var parentWizard=$(this).closest('.wizard')
+      console.log(direction)
+      var nextActive
+      if(direction == 'next'){
+        nextActive = parentWizard.find('.active').removeClass('active').next().addClass('active')
+      }else{
+        nextActive = parentWizard.find('.active').removeClass('active').prev().addClass('active')
+      }
+      if(nextActive.prev().size()==0){
+        parentWizard.find('.button-previous').hide()
+      }else if(nextActive.next().size()==0){
+        parentWizard.find('.button-next').hide()
+      }else{
+        parentWizard.find('.button-next,.button-previous').show()
+      }
+        
+    }
 
-  }
-})
+  $(document).on('click', '.wizard .button-next, .wizard .button-previous', function (e) {
+    e.preventDefault()
+    var element= $(this)
+    if(element.hasClass('button-next')){
+      element.MT_wizard('next')
+    }else{
+      element.MT_wizard('previous')
+    }
+    
+  })
+
+}(jQuery);
