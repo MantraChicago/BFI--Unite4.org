ed69527b390acecdf335bafde5bc4db93bb87025
Eddie Anderson <ejanderson4@gmail.com>
2013-07-15 18:35:36 -0500

    redirect to current page after login

--- app/assets/javascripts/components/login_modal.js
+++ app/assets/javascripts/components/login_modal.js
@@ -1,36 +1,14 @@
-Application.show_login_modal=function(){
+Application.show_login_modal=function(message){
   $('body').prepend('<div id="login_modal" class="reveal-modal small"></div>')
 
   $('#login_modal').foundation('reveal', 'open', {
     url: '/modals/login',
     success: function(data) {
       //hack
-      /*
       setTimeout(function(){
-        $('#login_modal form').submit(function(event){
-          
-          event.preventDefault()
-          var user_name= $(this).find('#user_email').val()
-          var password= $(this).find('#user_password').val()
-          var login_data={
-              login:user_name,
-              password:password
-          }
-
-          $.ajax({
-            url: '/users/sign_in.json',
-            type: 'POST',
-            dataType: 'json',
-            data: {user: login_data},
-            success: function(data, textStatus, xhr) {
-              console.log(data)
-              //called when successful
-            }
-          });
-
-        })   
+        $('#login_modal .message').html(message)
       },100)
-      */
+      
        
     },
     closed: function(){
