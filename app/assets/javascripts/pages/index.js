7a5f342e984d3b40e216b4874466637131478044
Eddie Anderson <ejanderson4@gmail.com>
2013-07-23 17:42:32 -0500

    home page take action form submits to city page now

--- app/assets/javascripts/pages/index.js
+++ app/assets/javascripts/pages/index.js
@@ -16,7 +16,11 @@ Application.pages.index=function(){
 
   $('.index-seach').submit(function(event){
     event.preventDefault();
-    var url="/charities-supporting-"+$(this).find('#cause_type_cause').val()+'?has_need='+$(this).find('[name="has_need"]').val()
+    var cause_type=$(this).find('#cause_type_cause').val()
+    var has_need= $(this).find('[name="has_need"]').val()
+    var city = $(this).find('[name="city"]').val()
+
+    var url="/charities-in-"+city+'?has_need='+$(this).find('[name="has_need"]').val()+'&cause_type_id='+cause_type
     window.location=url
   })
 
