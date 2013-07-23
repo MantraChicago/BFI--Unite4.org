595cb55c2cad31ac0e6151d087e51601409b5d1c
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-23 02:57:30 -0500

    improving snapshotting

--- lib/recipes/snapshots.rb
+++ lib/recipes/snapshots.rb
@@ -5,11 +5,26 @@ Capistrano::Configuration.instance.load do
       run "mkdir -p #{ shared_path }/backup"
     end
 
+    desc "Syncs your local development environment with data / files from production"
+    task :all do
+      prepare
+      system_folder.download
+      system_folder.restore
+      database.download
+      database.restore
+    end
+
     namespace :system_folder do
       desc "Downloads an archive of the system folder"
       task :download do
         run "cd #{ shared_path }/system && tar -c * |gzip -c > #{ shared_path }/backup/system.tar.gz"
         get "#{ shared_path }/backup/system.tar.gz", "snapshots/system.tar.gz"
+      end
+
+      desc "Restores your system folder from the snapshot"
+      task :restore do
+        project_root = Dir.pwd()
+        system "cd #{ project_root }/public/system && rm -rf * && tar zxvf #{ project_root }/snapshots/system.tar.gz"
       end
     end
 
@@ -22,6 +37,15 @@ Capistrano::Configuration.instance.load do
           ch.send_data "#{db_pass}\n" if out=~ /^Password:/
         end
         get db_remote_file, db_local_file
+      end
+
+      desc "Restores your local db from the snapshot"
+      task :restore do
+        load_db_config
+        download unless File.exists?(db_local_file)
+        cfg = local_db_config
+        system "bundle exec rake db:drop db:create"
+        system "psql -U #{ cfg['username'] } -d #{ cfg['database'] } -f snapshots/bfi_production-dump.sql"
       end
     end
   end
@@ -37,6 +61,10 @@ Capistrano::Configuration.instance.load do
     set(:db_pass) { cfg["password"] }
     set(:db_host) { cfg["host"] }
     set(:db_name) { cfg["database"] }
+  end
+
+  def local_db_config
+     YAML::load(ERB.new(IO.read(File.join('config', 'database.yml'))).result)['development']
   end
 
   def remote_db_config
