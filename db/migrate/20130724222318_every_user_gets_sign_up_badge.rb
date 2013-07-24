ee36fd9a83c5b5708994b048ce60d342a45f547f
Eddie Anderson <ejanderson4@gmail.com>
2013-07-24 17:45:06 -0500

    remove seed from migration

--- db/migrate/20130724222318_every_user_gets_sign_up_badge.rb
+++ db/migrate/20130724222318_every_user_gets_sign_up_badge.rb
@@ -1,6 +1,5 @@
 class EveryUserGetsSignUpBadge < ActiveRecord::Migration
   def change
-    Rake::Task['db:seed'].invoke #get badges in
 
     User.all.each do |user|
       user.award_badge 'Signed Up'
