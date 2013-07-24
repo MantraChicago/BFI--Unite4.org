5cf52db665aec2ae023bb89f9aa03a6415a51561
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-07-24 16:29:01 -0500

    checkpoint

--- spec/factories/contributions.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :contribution do
-    association(:user)
-    association(:cause, :skip_default_location=>true)
-    association(:need, :volunteers)
-  end
-end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,11 +2,31 @@ require "spec_helper"
 
 describe Campaign do
   let(:cause) { create(:cause, :skip_default_location => true) }
-  let(:campaign) { cause.active_campaign }
+  let(:proxy) { Unite::NeedsFulfillmentProxy }
+  let(:need) { create(:need, :social, cause: cause) }
+  let(:user) { create(:user) }
+  let(:follower) { proxy.new(user,need).fulfill! }
+
+
+  let(:campaign) do
+    campaign = cause.active_campaign
+    campaign.update_attributes(:desired_state=>"20")
+    campaign
+  end
 
   it "should know how many days to go" do
     campaign.end_date = 5.days.from_now
     campaign.days_to_go.should == 5
+  end
+
+  describe "Incrementing Progress" do
+    it "should attempt to update the campaign progress upon creation of a fulfillment" do
+    end
+
+    it "should increment the campaign progress" do
+      follower.should be_valid
+      campaign.percent_complete.should == 5
+    end
   end
 end
 
