cb86055f9f5801b5543f3e0d3c4e8bf5b0e22f45
Alex Smith <aosmith@gmail.com>
2013-07-25 00:27:56 -0500

    production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 require 'rvm/capistrano'
 
-server '192.34.61.72', :app, :web, :db, :primary => true
+server '192.241.149.12', :app, :web, :db, :primary => true
 set :user, 'deploy'
-set :deploy_to, '/home/deploy/rails_apps/bfi'
+set :deploy_to, '/home/deploy/rails_apps/unite4.org'
 set :use_sudo, false
 
 
