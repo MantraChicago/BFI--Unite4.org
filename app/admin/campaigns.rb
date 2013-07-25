112983c57d4324b2f5838aa90d66fd9c7dfeafc4
Eddie Anderson <ejanderson4@gmail.com>
2013-07-25 13:03:55 -0500

    active admin better interface for campaigns

--- app/admin/campaigns.rb
+++ app/admin/campaigns.rb
@@ -1,2 +1,22 @@
 ActiveAdmin.register Campaign do
+  form  do |f|
+     f.inputs "Details" do
+
+      f.input :cause
+      f.input :need
+      #f.form_buffers.last << content_tag(:li, content_tag(:label, 'Need') + content_tag(:div, '', :class =>'input_holder'))
+      f.input :active, :default =>true
+      f.input :goal_summary
+      f.input :timefame_description
+      f.input :start_date 
+      f.input :end_date
+      f.input :percent_complete
+      f.input :desired_state
+      f.input :current_state
+
+    end
+    f.buttons
+   end
 end
+
+
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,3 +2,37 @@
 
 //= require active_admin/base
 //= require jquery-ui
+
+var causes_data
+
+$(document).ready(function(){
+  $.getJSON('/api/v1/causes/details',function(data){
+  causes_data=data
+   $('#campaign_cause_id').change(function(){
+      hide_noncause_needs($(this).val())
+    }).change()
+  })
+
+})
+
+
+var hide_noncause_needs = function(cause_id){
+  var cause
+  for(var i in causes_data){
+    if(causes_data[i].id==cause_id){
+      cause = causes_data[i]
+    }
+  }
+
+  $('#campaign_need_id option').each(function(){
+    var hide=true
+    for(var i in cause.needs){
+      var need=cause.needs[i]
+      if(need.id==$(this).attr('value')){
+        hide=false
+      }
+    }
+    $(this).attr('disabled',hide)
+    
+  })
+}
