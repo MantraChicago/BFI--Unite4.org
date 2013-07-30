304742bcd63819bb26c31aff529b8b4d0015c96c
Eddie Anderson <ejanderson4@gmail.com>
2013-07-30 12:46:08 -0500

    fixing active admin for campaigns

--- app/admin/campaigns.rb
+++ app/admin/campaigns.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Campaign do
       #f.form_buffers.last << content_tag(:li, content_tag(:label, 'Need') + content_tag(:div, '', :class =>'input_holder'))
       f.input :active, :default =>true
       f.input :goal_summary
-      f.input :timefame_description
+      f.input :timeframe_description
       f.input :start_date 
       f.input :end_date
       f.input :percent_complete
