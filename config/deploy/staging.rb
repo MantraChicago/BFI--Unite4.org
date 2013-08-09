3479375b8608492eb3fd7d3fd324ce4d30b7b8be
Alex Smith <aosmith@gmail.com>
2013-08-09 15:01:47 -0500

    updated yamls, WIP deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,10 +6,6 @@ server 'staging.unite4.org', :app, :web, :db, :primary => true
 set :user, 'deploy'
 set :deploy_to, '/home/deploy/rails_apps/unite4.org'
 set :use_sudo, false
-set :rails_env, :staging
-
-#set :shell, '/bin/zsh'
-#set :default_shell, '/bin/zsh'
 
 load 'deploy/assets'
 
