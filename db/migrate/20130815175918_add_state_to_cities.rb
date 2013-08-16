23c695f4d821a3b7b2175bc9fd73545f9236ae8e
Eddie Anderson <ejanderson4@gmail.com>
2013-08-16 16:01:04 -0500

    removing cities migration

--- db/migrate/20130815175918_add_state_to_cities.rb
+++
@@ -1,7 +0,0 @@
-class AddStateToCities < ActiveRecord::Migration
-  def change
-    add_column :cities, :state, :string
-    change_column :cities, :latitude, :float
-    change_column :cities, :longitude, :float
-  end
-end
