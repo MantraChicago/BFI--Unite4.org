4cc6c8fb6944ad24f3f6e369a2e542df0a13b624
Eddie Anderson <ejanderson4@gmail.com>
2013-08-22 13:54:19 -0500

    fixing contenteditable issue in Firefox

--- spec/features/need_modals/cash_donation_modal_spec.rb
+++ spec/features/need_modals/cash_donation_modal_spec.rb
@@ -19,13 +19,14 @@ describe 'cash_donation_modal', :js =>true do
   it 'The cash donation modal should open' do
       
     login_as(@user, :scope => :user)
-    
+    cash_donations_need= create(:need, :cash, cause: cause)
 
     visit "/causes/#{cash_donations_need.cause.slug}"
     
     first('[data-type-of-need="cash_donations"]').click
     modal_title='ank you for your interest in donating money'
     sleep 2 #waiting for modals to appear 
+    binding.pry
     page.should have_content modal_title
 
     fill_in 'donation_amount', :with => '20'
@@ -33,10 +34,9 @@ describe 'cash_donation_modal', :js =>true do
 
     expect('total').to have_content '$25'
 
-    visit "/causes/#{goods_need.cause.slug}"
-
     find('#modalWrapper .close-reveal-modal').click
     sleep 1 #waiting for modals to appear 
     page.should_not have_content modal_title
+
   end
 end
