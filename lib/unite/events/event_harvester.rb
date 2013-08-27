42bf994c6627f66f626f36a701ec19fb5a20a3ee
Eddie Anderson <ejanderson4@gmail.com>
2013-08-27 15:43:15 -0500

    new logo

--- lib/unite/events/event_harvester.rb
+++
@@ -1,9 +0,0 @@
-module Unite
-  module Events
-    # The `EventHarvester` processes entries from the event tracking log and then
-    # aggregates those events into stats, metrics, attributes them to users, whatever
-    class EventHarvester
-      
-    end
-  end
-end
