233b573f674161a21ce77a55684eaa004e8774c4
Eddie Anderson <ejanderson4@gmail.com>
2013-09-05 14:45:33 -0500

    fixing migrations for needs

--- db/migrate/20130905175956_updates_badges_and_cities.rb
+++ db/migrate/20130905175956_updates_badges_and_cities.rb
@@ -6,8 +6,17 @@ class UpdatesBadgesAndCities < ActiveRecord::Migration
     Need.all.each do |need| #make sure this is done
       if need.type_of_need
         need.type= need.type_of_need.singularize.classify+'Need'
-        need.save
       end
+      need.is_active=true
+      need.is_primary=false
+      need.save
+    end
+
+    Campaign.all.each do |campaign|
+      primary_need = campaign.need
+      primary_need.is_primary =true
+      primary_need.end_date = Time.zone.now + 1.month
+      primary_need.save
     end
   end
 end
