3b51c01e1628541af079a4ce46348af5fec837e3
Eddie Anderson <ejanderson4@gmail.com>
2013-09-05 12:51:24 -0500

    colored badges #230 #229

--- db/migrate/20130815175918_add_state_to_cities.rb
+++ db/migrate/20130815175918_add_state_to_cities.rb
@@ -2,5 +2,7 @@ class AddStateToCities < ActiveRecord::Migration
   def change
     change_column :cities, :latitude, :float
     change_column :cities, :longitude, :float
+
+    City.create_defaults
   end
 end
