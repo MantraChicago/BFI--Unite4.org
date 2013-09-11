3a2b57ecb3c5d1f2f5446952a59136bbd8ea6128
Eddie Anderson <ejanderson4@gmail.com>
2013-09-11 17:31:56 -0500

    signing in redirects you back to where you were #288

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => request.protocol)
-    if request.referer == sign_in_url || resource.class.name== 'User' || (request.referer && request.referer.match(/\/users\//) )
+    if request.referer == sign_in_url || (request.referer && request.referer.match(/\/users\//) )
       root_path
     else
       stored_location_for(resource) || request.referer || root_path
     end
+    #binding.pry
   end
 
   def cause_edit_auth
