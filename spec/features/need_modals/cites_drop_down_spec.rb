fbe2c3d949cf1be40a9361b2af63712d5611b866
Ozzy <ochagata@benchprep.com>
2013-09-12 15:33:44 -0500

    optimized find causes modal spec

--- spec/features/need_modals/cites_drop_down_spec.rb
+++ spec/features/need_modals/cites_drop_down_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 require 'capybara/rspec'
 
-describe 'find_causes_modal', :js =>true do
+describe 'cites_drop_down', :js =>true do
   include Warden::Test::Helpers
   Warden.test_mode!
 
-  
+  before :all do
+    CauseType.create_defaults
+    @user = FactoryGirl.create(:user)
+  end
+
+  let(:cause) { create(:cause) }
+
+  let(:goods_need) { create(:need, :goods, cause: cause)}
+
   it 'Find causes modal should open' do
 
     login_as(@user, :scope => :user)
     modal_title = 'What are you passionate about'
     visit "/"
     find("#causes_nav").click
-    sleep 2
+    sleep 5
     page.should have_content modal_title
     find("a.close-reveal-modal").click
     page.should_not have_content modal_title
