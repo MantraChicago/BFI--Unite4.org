b9710fe4585c86b115e4703def0f9b7e8aca159f
Ozzy <ochagata@benchprep.com>
2013-09-12 15:27:46 -0500

    finalized find_causes_spec

--- spec/features/need_modals/find_causes_modal_spec.rb
+++ spec/features/need_modals/find_causes_modal_spec.rb
@@ -17,8 +17,12 @@ describe 'find_causes_modal', :js =>true do
   it 'Find causes modal should open' do
 
     login_as(@user, :scope => :user)
-
+    modal_title = 'What are you passionate about'
     visit "/"
+    find("#causes_nav").click
+    sleep 5
     page.should have_content modal_title
+    find("a.close-reveal-modal").click
+    page.should_not have_content modal_title
   end
 end
