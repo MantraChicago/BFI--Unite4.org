7acb489e17556215cca6f36ba90ee53f19afd353
Ozzy <ochagata@benchprep.com>
2013-09-12 15:40:55 -0500

    implemented cities drop down spec

--- spec/features/need_modals/cites_drop_down_spec.rb
+++ spec/features/need_modals/cites_drop_down_spec.rb
@@ -5,24 +5,16 @@ describe 'cites_drop_down', :js =>true do
   include Warden::Test::Helpers
   Warden.test_mode!
 
-  before :all do
-    CauseType.create_defaults
-    @user = FactoryGirl.create(:user)
-  end
-
-  let(:cause) { create(:cause) }
-
-  let(:goods_need) { create(:need, :goods, cause: cause)}
-
-  it 'Find causes modal should open' do
+  
+  it 'Cities Drop Down should open' do
 
     login_as(@user, :scope => :user)
-    modal_title = 'What are you passionate about'
+    modal_title = 'Browse All Cities'
     visit "/"
-    find("#causes_nav").click
-    sleep 5
+    find("a.popover_tigger").click
+    sleep 2
     page.should have_content modal_title
-    find("a.close-reveal-modal").click
+    find("a.popover_tigger").click
     page.should_not have_content modal_title
   end
 end
