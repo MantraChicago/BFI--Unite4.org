4ad0b99a53a329f57080b2aaedc18be896fa868c
Eddie Anderson <ejanderson4@gmail.com>
2013-09-16 15:50:24 -0500

    fixing bugs found in production

---
+++ db/migrate/20130916203303_add_default_cities.rb
@@ -0,0 +1,14 @@
+class AddDefaultCities < ActiveRecord::Migration
+  def change
+    City.create_defaults
+    Cause.all.each do |cause|
+      begin
+        city = City.find_or_create_by_slug cause.city_slug
+        cause.cities << city
+        cause.save
+      rescue
+        puts 'Error saving city'
+      end
+    end
+  end
+end
