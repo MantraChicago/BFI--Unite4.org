780fb5a076b150ab8aaa1df368e53944f214ee88
Eddie Anderson <ejanderson4@gmail.com>
2013-09-19 15:52:26 -0500

    active admin interface fixes

--- app/admin/badges.rb
+++ app/admin/badges.rb
@@ -1,7 +1,16 @@
 ActiveAdmin.register Badge do
+
   form :html => { :enctype => "multipart/form-data" } do |f|
-    f.input :name
-    f.input :description
-    f.input :image, :as => :file
-  end
+     f.inputs "Details" do
+      f.input :name
+      f.input :description
+      f.input :description_after
+      f.input :description_before
+      f.input :sub_text
+      f.input :is_active
+      f.input :image, :as => :file
+      
+    end
+    f.buttons
+   end  
 end
--- app/admin/needs.rb
+++ app/admin/needs.rb
@@ -3,15 +3,16 @@ form :html => { :enctype => "multipart/form-data" } do |f|
      f.inputs "Details" do
 
       f.input :name
+      f.input :is_primary
+      f.input :is_active
 
       f.input :cause
+
       f.input :description
-      f.input :location
       
-      
-      f.input :address
-      f.input :start_date
       f.input :end_date
+      f.input :current_state
+
       
 
       f.input :picture, :as => :file, :hint => f.template.image_tag(f.object.picture.url(:medium))
